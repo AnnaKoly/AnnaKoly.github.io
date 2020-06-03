2395d39dae7448cc07519892973f6b038727312e
AnnaKoly <annakoliadzhyn@gmail.com>
2020-06-03 15:05:03 +0300

    curr

--- JS/chess Board/script.js
+++ JS/chess Board/script.js
@@ -1,19 +1,22 @@
 var content = document.getElementById("content")
 
-for (var i = 1; i <= 8; i++) {
-    var newRow = document.createElement ("div");
+for (var i = 1; i <= 10; i++) {
+    var newRow = document.createElement("div");
     
-    
-    newRow.setAttribute ("class", "row");
-    for (var j = 1; j <= 8; j++) {
+
+    newRow.setAttribute("class", "row");
+    for (var j = 1; j <= 10; j++) {
         var newCell = document.createElement("div");
         newCell.setAttribute("class", "cell");
         newRow.appendChild(newCell);
-        if (i%2 == j%2) {
+        if (i % 2 == j % 2) {
             newCell.setAttribute("class", "white cell")
         } else {
-            newCell.setAttribute("class", "black cell")}
-    } 
-    
+            newCell.setAttribute("class", "black cell")
+            //var arrLetters = ['A', 'B', 'C', 'D', 'E', 'H', 'G', 'H', 'I', 'J']
+        }
+            
+    }
+
     content.appendChild(newRow);
 }
